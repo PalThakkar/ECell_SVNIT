c0db38c4082bd15fcd6b018cff0825520975f6b2
S R Jay <srjaykikani@gmail.com>
2024-07-13 20:11:36 +0530

    Initial commit

---
+++ src/config/site.js
@@ -0,0 +1,37 @@
+export function constructMetadata({
+  title = "E-Cell SVNIT ",
+  description = "E-Cell SVNIT is an open-source website.",
+  image = "/agency.PNG",
+  icons = "/favicon.ico",
+  noIndex = false,
+}) {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@KING_IN_JUNGLE",
+    },
+    icons,
+    metadataBase: new URL("https://agency-website-E-Cell SVNIT.vercel.app/"),
+    themeColor: "#FFF",
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
+}
