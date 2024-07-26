24a50fac55cfb1a47922942b4bc2078df9c9c39a
S R Jay <srjaykikani@gmail.com>
2024-07-26 22:25:34 +0530

    base

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,16 +1,27 @@
 "use client";
 import { usePathname } from "next/navigation";
-import { useEffect, useId, useState } from "react";
+import { useEffect, useId, useRef, useState } from "react";
 import { motion, MotionConfig, useReducedMotion } from "framer-motion";
 import Container from "./Container";
 import Link from "next/link";
 import Logo from "./Logo";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { IoMdClose } from "react-icons/io";
 import Button from "./Button";
+import clsx from "clsx";
 import Offices from "./Offices";
 import SocialMedia from "./SocialMedia";
 import Footer from "./Footer";
 
-const Header = ({ invert = false }) => {
+const Header = ({
+  panelId,
+  invert = false,
+  icon: Icon,
+  expanded,
+  onToggle,
+  toggleRef,
+}) => {
+  // Container
   return (
     <Container>
       <div className="flex items-center justify-between">
@@ -24,12 +35,32 @@ const Header = ({ invert = false }) => {
           <Button href={"/contact"} invert={invert}>
             Contact us
           </Button>
+          <button
+            ref={toggleRef}
+            type="button"
+            onClick={onToggle}
+            aria-expanded={expanded.toString()}
+            aria-controls={panelId}
+            className={clsx(
+              "group -m-2.5 rounded-full p-2.5 transition",
+              invert ? "hover:bg-white/10" : "hover:bg-neutral-950/10"
+            )}
+            aria-label="Toggle navigation"
+          >
+            <Icon
+              className={clsx(
+                "h-6 w-6",
+                invert
+                  ? "fill-white group-hover:fill-neutral-200"
+                  : "fill-neutral-950 group-hover:fill-neutral-700"
+              )}
+            />
+          </button>
         </div>
       </div>
     </Container>
   );
 };
-
 const NavigationRow = ({ children }) => {
   return (
     <div className="even:mt-px sm:bg-neutral-950">
@@ -68,21 +99,70 @@ const Navigation = () => {
 };
 
 const RootLayoutInner = ({ children }) => {
+  const panelId = useId();
+  const [expanded, setExpanded] = useState(false);
+  const openRef = useRef();
+  const closeRef = useRef();
+  const navRef = useRef();
   const shouldReduceMotion = useReducedMotion();
+  useEffect(() => {
+    function onClick(event) {
+      if (event.target.closest("a")?.href === window.location.href) {
+        setExpanded(false);
+      }
+    }
+    window.addEventListener("click", onClick);
+
+    return () => {
+      window.removeEventListener("click", onClick);
+    };
+  }, []);
   return (
     <MotionConfig transition={shouldReduceMotion ? { duration: 0 } : undefined}>
       <header>
-        <div className="absolute left-0 right-0 top-2 z-40 pt-14">
+        <div
+          className="absolute left-0 right-0 top-2 z-40 pt-14"
+          aria-hidden={expanded ? "true" : undefined}
+          inert={expanded ? "" : undefined}
+        >
           {/* Header */}
-          <Header />
+          <Header
+            panelId={panelId}
+            icon={HiMenuAlt4}
+            toggleRef={openRef}
+            expanded={expanded}
+            onToggle={() => {
+              setExpanded((expanded) => !expanded);
+              window.setTimeout(() =>
+                closeRef.current?.focus({ preventScroll: true })
+              );
+            }}
+          />
         </div>
-      </header>
-      <motion.div
-        layout
-        className="relative z-50 overflow-hidden bg-neutral-950 pt-2"
-      >
-        <motion.div layout className="bg-neutral-800">
-          <div className="bg-neutral-950 pb-16 pt-14">
+        <motion.div
+          layout
+          id={panelId}
+          style={{ height: expanded ? "auto" : "0.5rem" }}
+          className="relative z-50 overflow-hidden bg-neutral-950 pt-2"
+          aria-hidden={expanded ? undefined : "true"}
+          inert={expanded ? undefined : ""}
+        >
+          <motion.div layout className="bg-neutral-800">
+            <div ref={navRef} className="bg-neutral-950 pb-16 pt-14">
+              <Header
+                invert
+                panelId={panelId}
+                icon={IoMdClose}
+                toggleRef={closeRef}
+                expanded={expanded}
+                onToggle={() => {
+                  setExpanded((expanded) => !expanded);
+                  window.setTimeout(() =>
+                    openRef.current?.focus({ preventScroll: true })
+                  );
+                }}
+              />
+            </div>
             {/* Navigation */}
             <Navigation />
             <div className="relative bg-neutral-950 before:absolute before:inset-x-0 before:top-0 before:h-px before:bg-neutral-800">
@@ -106,9 +186,9 @@ const RootLayoutInner = ({ children }) => {
                 </div>
               </Container>
             </div>
-          </div>
+          </motion.div>
         </motion.div>
-      </motion.div>
+      </header>
       <motion.div
         layout
         style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
