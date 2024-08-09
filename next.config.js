09b14ac605dc9dda470e3e6ec4dd8cbad926600e
S R Jay <srjaykikani@gmail.com>
2024-08-09 22:42:47 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,  // Enables React's Strict Mode for better development practices
+  
+    images: {
+      domains: ['ecellsvnit.com'],  // Optimizes images from your domain
+      formats: ['image/avif', 'image/webp'],  // Enables modern image formats for better compression
+    },
+  
+    webpack: (config) => {
+      // Code splitting optimization: Ensures common chunks are extracted and loaded only when necessary
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 70000,
+      };
+  
+      // Additional performance optimizations can be applied here if needed
+  
+      return config;
+    },
+  
+    experimental: {
+      scrollRestoration: true,  // Improves the user experience by preserving scroll position between navigation
+    },
+  };
+  
+  module.exports = nextConfig;
+  
