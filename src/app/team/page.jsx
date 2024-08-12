e840a6168fac94988a8a89de54994fb3cb49f7fa
S R Jay <srjaykikani@gmail.com>
2024-08-12 14:40:59 +0530

    Update page.jsx

--- src/app/team/page.jsx
+++ src/app/team/page.jsx
@@ -34,22 +34,114 @@ export const metadata = {
 
 const teamMembers = {
   chiefExecutive: [
-    // ... (team members data)
+    {
+      photo: '/jeet.jpg',
+      name: 'Jeet Ariwala',
+      position: 'Chief Executive',
+      linkedin: 'https://www.linkedin.com/in/jeet-ariwala-152243256',
+      instagram: 'https://www.instagram.com/jeet_ariwala21',
+    },
+    {
+      photo: '/kartik.jpg',
+      name: 'Kartik Srivastava',
+      position: 'Chief Executive',
+      linkedin: 'https://www.linkedin.com/in/kartik-srivastava-b46b561b7',
+      instagram: 'https://www.instagram.com/__.the.prodigal.son.__',
+    },
+    {
+      photo: '/kashish.jpg',
+      name: 'Kashish Sharma',
+      position: 'Chief Executive',
+      linkedin: 'https://www.linkedin.com/in/kashish-sharma-545774215',
+      instagram: 'https://www.instagram.com/kashishhh__11',
+    },
+    {
+      photo: '/omraa.jpg',
+      name: 'Om Ramanuj',
+      position: 'Chief Executive',
+      linkedin: 'https://www.linkedin.com/in/om-ramanuj-511501266',
+      instagram: 'https://www.instagram.com/ramanuj_om',
+    },
   ],
   technical: [
-    // ... (team members data)
+    {
+      photo: '/srjay.jpg',
+      name: 'S R Jay Kikani',
+      position: 'Head',
+      linkedin: 'https://www.linkedin.com/in/srjaykikani',
+      instagram: 'https://www.instagram.com/_srjay',
+    },
+    {
+      photo: '/shambhavi.jpg',
+      name: 'Shambhavi Shinde',
+      position: 'Co-head',
+      linkedin: 'https://www.linkedin.com/in/shambhavishinde',
+      instagram: 'https://www.instagram.com/shmbhvi',
+    },
   ],
   mediaAndPublicity: [
-    // ... (team members data)
+    {
+      photo: '/dangar.jpg',
+      name: 'Ronak Dangar',
+      position: 'Head',
+      linkedin: 'https://www.linkedin.com/in/ronak-dangar',
+      instagram: 'https://www.instagram.com/ronak_dangar_04',
+    },
+    {
+      photo: '/om-p.jpg',
+      name: 'Om Panchal',
+      position: 'Co-Head',
+      linkedin: 'https://in.linkedin.com/in/om-panchal-136410257',
+      instagram: 'https://www.instagram.com/om_panchal_op7',
+    },
   ],
   events: [
-    // ... (team members data)
+    {
+      photo: '/tripathi.jpg',
+      name: 'Pritish Tripathi',
+      position: 'Head',
+      linkedin: 'https://www.linkedin.com/in/pritish-tripathi-362006271',
+      instagram: 'https://www.instagram.com/tripathipritish',
+    },
+    {
+      photo: '/tanish.jpg',
+      name: 'Tanish Panchal',
+      position: 'Co-head',
+      linkedin: 'https://www.linkedin.com/in/tanish2311',
+      instagram: 'https://www.instagram.com/ttan_ishh',
+    },
   ],
   startupTeam: [
-    // ... (team members data)
+    {
+      photo: '/ridhayu.jpg',
+      name: 'Gosai Ridhayu',
+      position: 'Head',
+      linkedin: 'https://www.linkedin.com/in/ridhayu-gosai-4b063a280',
+      instagram: 'https://www.instagram.com/ridhayu_gosai_28',
+    },
+    {
+      photo: '/sp.jpg',
+      name: 'Soumyashree Parida',
+      position: 'Co-head',
+      linkedin: 'https://www.linkedin.com/in/soumyashreeparida785',
+      instagram: 'https://www.instagram.com/pvtt_soumya',
+    },
   ],
   publicRelations: [
-    // ... (team members data)
+    {
+      photo: '/zala.jpg',
+      name: 'Nanviya Zala',
+      position: 'Head',
+      linkedin: 'https://www.linkedin.com/in/nanviya-zala-108324306',
+      instagram: 'https://www.instagram.com/notnanviazala',
+    },
+    {
+      photo: '/parmar.jpg',
+      name: 'Krish Parmar',
+      position: 'Co-Head',
+      linkedin: 'https://www.linkedin.com/in/krish-parmar-a30211258',
+      instagram: 'https://www.instagram.com/krishh_.003',
+    },
   ],
 };
 
