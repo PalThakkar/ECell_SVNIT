b033b597a8df9f3f33ccdfe688299da47cae3aa5
S R Jay <srjaykikani@gmail.com>
2024-08-12 18:32:30 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -63,7 +63,7 @@ const Footer = () => {
         </div>
         <div className="mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12">
           <Link href={"/"} aria-label="Home">
-            <Logo className="h-8 mb-16" fillOnHover />
+            <Logo className="h-25 mb-1 sm:mb-16" fillOnHover />
           </Link>
           <p className="text-sm text-neutral-700">
             Developed by{" "}
